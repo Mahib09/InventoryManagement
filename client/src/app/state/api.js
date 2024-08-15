5f57150a3a4618425c57d23d87f378a15142c1ea
Mahib09 <magarmahib@gmail.com>
2024-08-15 00:05:47 -0400

    Popular Products UI completed

--- client/src/app/state/api.js
+++ client/src/app/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [DashboardMetrics],
+  tagTypes: ["DashboardMetrics"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query({
       query: () => "/dashboard",
@@ -12,4 +12,4 @@ export const api = createApi({
   }),
 });
 
-export const { useGetDashboardmetricsQuery } = api;
+export const { useGetDashboardMetricsQuery } = api;
