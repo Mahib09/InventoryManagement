257963b3547da43f1cb7f3438ab45f8c9caadf35
Mahib09 <magarmahib@gmail.com>
2024-08-21 13:37:13 -0400

    Stat Cards added Dashboard Complete

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Package, TrendingDown, TrendingUp } from "lucide-react";
+import {
+  CheckCircle,
+  Package,
+  Tag,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
 import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
 import CardPurchaseSummary from "./CardPurchaseSummary";
@@ -33,8 +39,44 @@ const Dashboard = () => {
         ]}
         primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
       />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <StatCard
+        title="Dues & Pending Orders"
+        dateRange="22 - 29 July 2024"
+        details={[
+          {
+            title: "Dues",
+            amount: "250.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Pending Orders",
+            amount: "147",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+      />
+      <StatCard
+        title="Sales & Discount"
+        dateRange="22 - 29 July 2024"
+        details={[
+          {
+            title: "Sales",
+            amount: "1000.00",
+            changePercentage: 20,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Discount",
+            amount: "200.00",
+            changePercentage: -10,
+            IconComponent: TrendingDown,
+          },
+        ]}
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+      />
     </div>
   );
 };
