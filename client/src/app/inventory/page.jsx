27f1b69774aa6a1f2e2ae75ae23f1f03849ce87d
Mahib09 <magarmahib@gmail.com>
2024-08-21 17:33:22 -0400

    Inventory Page Made

--- client/src/app/inventory/page.jsx
+++ client/src/app/inventory/page.jsx
@@ -1,7 +1,32 @@
 "use client";
 
+import { DataGrid } from "@mui/x-data-grid";
 import Header from "../(components)/Header";
 import { useGetProductsQuery } from "../state/api";
+const columns = [
+  { field: "productId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Product Name", width: 200 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? row.rating : "N/A"),
+  },
+  {
+    field: "stockQuantity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
+];
 
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
@@ -20,6 +45,13 @@ const Inventory = () => {
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border dorder-gray-200 mt-5 !textgray-700"
+      />
     </div>
   );
 };
