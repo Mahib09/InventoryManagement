061709a41b88164885bbb0592f0c0ec0fd324da5
Mahib09 <magarmahib@gmail.com>
2024-08-23 17:28:15 -0400

    cors config

--- server/src/index.js
+++ server/src/index.js
@@ -12,20 +12,31 @@ const expenseRoutes = require("./routes/expenseRoutes");
 // Configurations
 dotenv.config();
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
-// routes
+// CORS Configuration
+const corsOptions = {
+  origin: "*", // Allow requests from any origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Include credentials (e.g., cookies) if needed
+};
+
+app.use(cors(corsOptions));
+
+// Routes
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
-// server
+
+// Server
 const port = process.env.PORT || 3001;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
